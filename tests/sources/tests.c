249fc31a5ac2c903a3b115ecc2447954b5d9ad42
hrn <prevost.corentin@yahoo.com>
2019-08-29 12:40:41 +0200

    [+] #13 launch specific test suite or test belonging to a suite by command line arguments

--- tests/sources/tests.c
+++ tests/sources/tests.c
@@ -7,13 +7,27 @@ void init_test_suites() {
     init_midifile_suite();
 }
 
-int main() {
+int main(int argc, char **argv) {
     CU_ErrorCode registry_initialization_error_code = CU_initialize_registry();
     if (CUE_SUCCESS != registry_initialization_error_code) {
         return CU_get_error();
     }
     init_test_suites();
     CU_basic_set_mode(CU_BRM_VERBOSE);
-    CU_basic_run_tests();
+    if (argc > 1) {
+        const char* suite_name = argv[1];
+        CU_pSuite test_suite = CU_get_suite(suite_name);
+        if (argc == 2) {
+            CU_basic_run_suite(test_suite);
+        } else {
+            for(int i=2; i < argc; i++) {
+                char* test_name = argv[i];
+                CU_pTest test_to_run = CU_get_test(test_suite, test_name);
+                CU_basic_run_test(test_suite, test_to_run);
+            }
+        }
+    } else {
+        CU_basic_run_tests();
+    }
     return CU_get_error();
 }
